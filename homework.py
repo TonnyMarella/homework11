a93a83574b3d314cb1f49ba371ef9824c5d4c5c3
KapOo <temazubkov02@gmail.com>
2022-08-29 20:34:14 +0300

    iterator complete

--- homework.py
+++ homework.py
@@ -13,6 +13,12 @@ class AddressBook(UserDict, Field):
     def add_record(self):
         self.date[Record.name.value] = Record.phone.value
 
+    def iterator(self, n):
+        result = []
+        for key, value in self.date.items():
+            result.append({key: value})
+        return result[:n]
+
 
 class Name(Field):
     def __init__(self, name):
@@ -48,35 +54,46 @@ class Record(Field):
                 Record.phone = Phone(new_phone)
 
 
-def main():
-    start = AddressBook()
-    record = Record()
-
-    while True:
-        a = input().lower()
-        if a == '.':
-            break
-        elif a in ("good bye", "close", "exit"):
-            print("Good Bye!")
-            break
-        elif a == 'hello':
-            print('How can I help you?')
-        elif a == 'show all':
-            print(start.date)
-        elif a.split()[0] == 'add':
-            record.add_contact(a.split()[1], a.split()[2:])
-            start.add_record()
-        elif a.split()[0] == 'change':
-            record.edit_contact(start, a.split()[1], a.split()[2:])
-            start.add_record()
-        elif a.split()[0] == 'delete':
-            need_to_delete = ''
-            for key, value in start.date.items():
-                if key == a.split()[1]:
-                    need_to_delete = a.split()[1]
-            if need_to_delete:
-                del start.date[need_to_delete]
+# def main():
+#     start = AddressBook()
+#     record = Record()
+#
+#     while True:
+#         a = input().lower()
+#         if a == '.':
+#             break
+#         elif a in ("good bye", "close", "exit"):
+#             print("Good Bye!")
+#             break
+#         elif a == 'hello':
+#             print('How can I help you?')
+#         elif a == 'show all':
+#             print(start.date)
+#         elif a.split()[0] == 'add':
+#             record.add_contact(a.split()[1], a.split()[2:])
+#             start.add_record()
+#         elif a.split()[0] == 'change':
+#             record.edit_contact(start, a.split()[1], a.split()[2:])
+#             start.add_record()
+#         elif a.split()[0] == 'delete':
+#             need_to_delete = ''
+#             for key, value in start.date.items():
+#                 if key == a.split()[1]:
+#                     need_to_delete = a.split()[1]
+#             if need_to_delete:
+#                 del start.date[need_to_delete]
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    start = AddressBook()
+    record = Record()
+    record.add_contact("Artem", '123123')
+    start.add_record()
+    record.add_contact("Dima", '123123')
+    start.add_record()
+    record.add_contact("ADasd", '123123')
+    start.add_record()
+    record.add_contact("dsad", '123123')
+    start.add_record()
+    print(start.iterator(3))
