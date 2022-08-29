4342ad2fb56a1c4aed62741389fb75ba3e711afb
KapOo <temazubkov02@gmail.com>
2022-08-29 20:24:14 +0300

    start

---
+++ homework.py
@@ -0,0 +1,82 @@
+from collections import UserDict
+
+
+class Field:
+    pass
+
+
+class AddressBook(UserDict, Field):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.date = {}
+
+    def add_record(self):
+        self.date[Record.name.value] = Record.phone.value
+
+
+class Name(Field):
+    def __init__(self, name):
+        self.value = name
+
+
+class Phone(Field):
+    def __init__(self, phone=None):
+        self.value = phone
+
+
+class Record(Field):
+    name = None
+    phone = None
+
+    @staticmethod
+    def add_contact(new_name, new_phone=None):
+        Record.name = Name(new_name)
+        Record.phone = Phone(new_phone)
+
+    @staticmethod
+    def delete_contact(cls, contact_name):
+        for key, value in cls.date.items():
+            if key == contact_name:
+                Record.name = Name(contact_name)
+                Record.phone = Phone()
+
+    @staticmethod
+    def edit_contact(cls, contact_name, new_phone):
+        for key, value in cls.date.items():
+            if key == contact_name:
+                Record.name = Name(contact_name)
+                Record.phone = Phone(new_phone)
+
+
+def main():
+    start = AddressBook()
+    record = Record()
+
+    while True:
+        a = input().lower()
+        if a == '.':
+            break
+        elif a in ("good bye", "close", "exit"):
+            print("Good Bye!")
+            break
+        elif a == 'hello':
+            print('How can I help you?')
+        elif a == 'show all':
+            print(start.date)
+        elif a.split()[0] == 'add':
+            record.add_contact(a.split()[1], a.split()[2:])
+            start.add_record()
+        elif a.split()[0] == 'change':
+            record.edit_contact(start, a.split()[1], a.split()[2:])
+            start.add_record()
+        elif a.split()[0] == 'delete':
+            need_to_delete = ''
+            for key, value in start.date.items():
+                if key == a.split()[1]:
+                    need_to_delete = a.split()[1]
+            if need_to_delete:
+                del start.date[need_to_delete]
+
+
+if __name__ == '__main__':
+    main()
